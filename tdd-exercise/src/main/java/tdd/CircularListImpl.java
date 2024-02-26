8b43a683f29ae00ee4f8e2bdb3e1de4e6f316da6
Angelo Tinti <angelo.tinti@studio.unibo.it>
2024-02-26 13:32:15 +0100

    Add isEmpty implementation to pass test

--- tdd-exercise/src/main/java/tdd/CircularListImpl.java
+++ tdd-exercise/src/main/java/tdd/CircularListImpl.java
@@ -24,7 +24,7 @@ public class CircularListImpl implements CircularList{
 
     @Override
     public boolean isEmpty() {
-        return true;
+        return this.list.isEmpty();
     }
 
     @Override
