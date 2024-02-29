ae003100ae851d13fb05e717baaa8fe149fd4a68
Angelo Tinti <angelo.tinti@studio.unibo.it>
2024-02-29 14:51:31 +0100

    Add CircularListIterable interface implentatation and methods for its implementation

--- tdd-exercise/src/main/java/tddstep2/CircularListIterable.java
+++ tdd-exercise/src/main/java/tddstep2/CircularListIterable.java
@@ -1,6 +1,17 @@
 package tddstep2;
 
-public interface CircularListIterable {
+import java.util.Iterator;
 
+public interface CircularListIterable {
     
-} 
+    public void add(int element);
+
+    public int size();
+
+    public boolean isEmpty();
+
+    public Iterator<Integer> forwardIterator();
+
+    public Iterator<Integer> backwardIterator();
+
+}
--- tdd-exercise/src/main/java/tddstep2/CircularListIterableImpl.java
+++ tdd-exercise/src/main/java/tddstep2/CircularListIterableImpl.java
@@ -1,5 +1,37 @@
 package tddstep2;
 
+import java.util.Iterator;
+
 public class CircularListIterableImpl implements CircularListIterable {
+
+    @Override
+    public void add(int element) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'add'");
+    }
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'size'");
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isEmpty'");
+    }
+
+    @Override
+    public Iterator<Integer> forwardIterator() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'forwardIterator'");
+    }
+
+    @Override
+    public Iterator<Integer> backwardIterator() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'backwardIterator'");
+    }
 
 }
