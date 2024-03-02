a41d70af35a22f79ba05457b8cd28c8a421db44e
Angelo Tinti <angelo.tinti@studio.unibo.it>
2024-03-02 18:48:40 +0100

    Add test for CircularListNext and implementation for filteredNext method

--- tdd-exercise/src/main/java/tddstep3/CircularListNextImpl.java
+++ tdd-exercise/src/main/java/tddstep3/CircularListNextImpl.java
@@ -7,10 +7,26 @@ import tdd.CircularListImpl;
 
 public class CircularListNextImpl extends CircularListImpl implements CircularListNext {
 
+    private int nextIndex;
+
     @Override
     public Optional<Integer> filteredNext(Predicate<Integer> predicate) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'filteredNext'");
+
+        Optional<Integer> elementFound = Optional.empty();
+
+        for (int i = 0; i < this.size() && elementFound.isEmpty(); i++) {
+            var listElement = Optional.of(this.list.get(this.getCircularIndex(i + nextIndex)));
+            if (predicate.test(listElement.get())) {
+                elementFound = listElement;   
+            }
+        }
+
+        return elementFound;
+
     }
-  
+
+    private int getCircularIndex(int index) {
+        return index % this.size();
+    }
+
 }
--- tdd-exercise/src/test/java/CircularListNextTest.java
+++ tdd-exercise/src/test/java/CircularListNextTest.java
@@ -1,3 +1,8 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Optional;
+import java.util.stream.IntStream;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -5,16 +10,37 @@ import tddstep3.CircularListNext;
 import tddstep3.CircularListNextImpl;
 
 public class CircularListNextTest extends CircularListTest {
-    private CircularListNext circularList;
+    private CircularListNext circularListNext;
 
     @BeforeEach
     @Override
     public void initializeList() {
         this.circularList = new CircularListNextImpl();
+        this.circularListNext = new CircularListNextImpl();
     }
 
     @Test
     public void testNextWithEmptyList() {
-        this.circularList.filteredNext(element -> element == 4);
+        assertEquals(Optional.empty(), this.circularListNext.filteredNext(element -> element == 0));
+    }
+
+    @Test
+    public void testNextWithoutElement() {
+        int elementToTest = 4;
+        this.circularListNext.add(0);
+        assertEquals(Optional.empty(), this.circularListNext.filteredNext(element -> element == elementToTest));
+    }
+
+    @Test
+    public void textNextWithMoreElements() {
+        int elementsQuantity = 10;
+        int elementToTest1 = 11;
+        int elementToTest2 = 12;
+        IntStream.range(0, elementsQuantity).forEach(element -> this.circularListNext.add(element)); 
+        this.circularListNext.add(elementToTest1);
+        this.circularListNext.add(elementToTest2);
+        assertEquals(Optional.of(elementToTest1), this.circularListNext.filteredNext(element -> element == elementToTest1));
+        assertEquals(Optional.of(elementToTest1), this.circularListNext.filteredNext(element -> element == elementToTest1));
+        assertEquals(Optional.of(elementToTest2), this.circularListNext.filteredNext(element -> element == elementToTest2));
     }
 }
